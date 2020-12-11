b6c0ff588bfbeec9bd1c9a99ff6b0a9cd37cff0e
Saad Shahid <saad.techgeek@gmail.com>
2020-12-12 04:56:01 +0500

    fix

--- functions/payment-wh/payment-wh.js
+++ functions/payment-wh/payment-wh.js
@@ -1,16 +1,15 @@
 // Docs on event and context https://www.netlify.com/docs/functions/#the-handler-method
-exports.handler = async (event, context) => {
-    try {
-      console.log("Payment Success Details: =>>", JSON.stringify(event.body));
-      return {
-        statusCode: 200,
-        body: JSON.stringify({ message: "Calling webhook function" }),
-        // // more keys you can return:
-        // headers: { "headerName": "headerValue", ... },
-        // isBase64Encoded: true,
-      }
-    } catch (err) {
-      return { statusCode: 500, body: err.toString() }
+exports.handler = async event => {
+  try {
+    console.log('payment Success details',JSON.stringify(event.body))
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ message: `calling webhook function` }),
+      // // more keys you can return:
+      // headers: { "headerName": "headerValue", ... },
+      // isBase64Encoded: true,
     }
+  } catch (error) {
+    return { statusCode: 500, body: error.toString() }
   }
-  
+}
